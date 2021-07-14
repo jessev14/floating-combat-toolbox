ccb260c1d45a978b035a743ee5a435ee06828fae
jessev14 <jessevo321@gmail.com>
2021-07-13 23:02:28 -0400

    reduce getTokenFromTokenID collision

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -8,9 +8,13 @@ export function getCombatfromCombatantID(combatantID) {
     return combat;
 }
 
-export function getTokenFromTokenID(tokenID) {
+export function getTokenFromTokenID(tokenID, tokenActorID = null) {
     const token = game.scenes.find(s => {
         for (const tkn of s.tokens.contents) {
+            if (tokenActorID) {  // optional comparison to avoid (reduce?) collision on the chance that two tokens have the same ID
+                if (tokenActorID !== tkn.actor.data._id) continue;
+            }
+
             if (tkn.data._id === tokenID) return true;
         }
     }).tokens.get(tokenID);
